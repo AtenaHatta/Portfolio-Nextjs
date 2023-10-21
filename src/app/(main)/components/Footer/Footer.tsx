115ecc26e5f9d3fd43ec394df06a2e27ebab0f08
AtenaHatta <atn041102@gmail.com>
2023-10-20 20:59:48 -0700

    ADDED footer responsive

--- src/app/(main)/components/Footer/Footer.tsx
+++ src/app/(main)/components/Footer/Footer.tsx
@@ -6,15 +6,15 @@ export default function Footer() {
   return (
     <div className="fixed bottom-0 -z-10 h-[400px] text-white bg-black w-full flex items-center text-center justify-center">
       <div className="flex-row">
-        <h1 className="text-sm md:text-4xl font-yesevaOne">ATENA HATTA</h1>
-        <p>All rights reserved &copy; {date} by Atena Hatta</p>
+        <h1 className="text-2xl md:text-4xl font-yesevaOne">ATENA HATTA</h1>
+        <p className="text-xs md:text-lg">All rights reserved &copy; {date} by Atena Hatta</p>
         <div className="flex items-center text-center justify-center">
           <a
             href="https://github.com/AtenaHatta/Portfolio-Nextjs"
-            className="flex items-center justify-center mt-5 p-1 w-[200px] border border-transparent hover:border-white rounded-md"
+            className="flex items-center justify-center mt-3 py-1 px-5 border border-transparent hover:border-white rounded-md"
           >
             <AiFillGithub className="text-5xl pr-2" />
-            <span className="text-xl">my Portfolio</span>
+            <span className="text-xl">Portfolio</span>
           </a>
         </div>
       </div>
