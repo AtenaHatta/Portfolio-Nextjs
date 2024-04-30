fc9712e28005b7e22c39302477698cd089870547
AtenaHatta <atn041102@gmail.com>
2024-04-29 22:19:44 -0700

    chore: removed experience section

--- src/app/(main)/components/Experience/Experience.tsx
+++ src/app/(main)/components/Experience/Experience.tsx
@@ -29,96 +29,6 @@ const Experience = ({ className = "" }) => {
               <div className="absolute hidden w-px h-full transform -translate-x-1/2 bg-purple-300 lg:block left-1/2"></div>
               <div className="space-y-2 lg:space-y-4">
                 <div className="flex flex-col items-center">
-                  <div className="flex items-center justify-start w-full mx-auto">
-                    <div className="w-full lg:w-1/2 lg:pr-8">
-                      <Link href="https://www.hannan-u.ac.jp/english/index.html">
-                        <div className="relative flex-1 mb-16 bg-white rounded shadow lg:mb-8  dark:bg-gray-900 hover:bg-purple-700">
-                          <div className="absolute inline-block w-4 overflow-hidden -translate-y-1/2 top-3 -right-4">
-                            <div className="hidden h-10 origin-bottom-left transform -rotate-45 bg-purple-600 shadow lg:block dark:bg-purple-500"></div>
-                          </div>
-                          <div className="relative z-20 p-6">
-                            <div className="absolute -top-4 -right[-30px] lg:top-0 lg:right-0 inline-block px-2 py-2 dark:bg-purple-500 bg-purple-600 lg:rounded-bl-md lg:rounded-tr-md">
-                              <span className="text-xs text-gray-100">
-                                Apr 2013 - 2014 Apr 2011 - 2015
-                              </span>
-                            </div>
-                            <p className="mt-4 mb-2 pt-4 text-xl font-bold text-gray-600 dark:text-gray-300">
-                              Hannan University
-                            </p>
-                            <p className="text-gray-700 dark:text-gray-400">
-                              International Tourism
-                            </p>
-                          </div>
-                        </div>
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="hidden absolute md:flex tems-center md:items-center justify-center w-8 h-8 transform -translate-x-1/2 -translate-y-4 bg-purple-100 border border-purple-600 rounded-full dark:border-gray-700 dark:bg-gray-700 left-1/2 sm:translate-y-0">
-                    <FaGraduationCap className="text-purple-600 dark:text-gray-400 text-xl" />
-                  </div>
-                </div>
-
-                <div className="flex flex-col items-center">
-                  <div className="flex items-center justify-end w-full mx-auto">
-                    <div className="w-full lg:w-1/2 lg:pl-8">
-                      <Link href="https://www.uwinnipeg.ca/">
-                        <div className="relative flex-1 mb-16 bg-white rounded shadow lg:mb-8 dark:bg-gray-900 hover:bg-purple-700">
-                          <div className="absolute inline-block w-4 overflow-hidden -translate-y-1/2 top-7 -left-4">
-                            <div className="hidden h-10 origin-top-right transform -rotate-45 bg-purple-600 dark:bg-purple-500 lg:block drop-shadow-lg"></div>
-                          </div>
-                          <div className="relative z-20 p-6">
-                            <div className="absolute -top-4 -left[-30px] lg:top-0 lg:left-0 inline-block px-2 py-2 dark:bg-purple-500 bg-purple-600 lg:rounded-br-md lg:rounded-tl-md">
-                              <span className="text-xs text-gray-100">
-                                Apr 2013 - 2014
-                              </span>
-                            </div>
-                            <p className="mt-4 mb-2 pt-4 text-xl font-bold text-gray-600 dark:text-gray-300">
-                              The University of Winnipeg
-                            </p>
-                            <p className="text-gray-700 dark:text-gray-400">
-                              1+ year study abroad in Canada
-                            </p>
-                          </div>
-                        </div>
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="hidden absolute md:flex items-center justify-center w-8 h-8 transform -translate-x-1/2 -translate-y-4 bg-purple-100 border border-purple-600 rounded-full dark:border-gray-700 dark:bg-gray-700 left-1/2 sm:translate-y-0">
-                    <AiOutlineGlobal className="text-purple-600 dark:text-gray-400 text-xl" />
-                  </div>
-                </div>
-
-                <div className="flex flex-col items-center">
-                  <div className="flex items-center justify-start w-full mx-auto">
-                    <div className="w-full lg:w-1/2 lg:pr-8">
-                      <Link href="https://www.hannan-u.ac.jp/english/index.html">
-                        <div className="relative flex-1 mb-16 bg-white rounded shadow lg:mb-8 dark:bg-gray-900 hover:bg-purple-700">
-                          <div className="absolute inline-block w-4 overflow-hidden -translate-y-1/2 top-3 -right-4">
-                            <div className="hidden h-10 origin-bottom-left transform -rotate-45 bg-purple-600 shadow lg:block dark:bg-purple-500"></div>
-                          </div>
-                          <div className="relative z-20 p-6">
-                            <div className="absolute -top-4 -right[-30px] lg:top-0 lg:right-0 inline-block px-2 py-2 dark:bg-purple-500 bg-purple-600 lg:rounded-bl-md lg:rounded-tr-md">
-                              <span className="text-xs text-gray-100 ">
-                                Apr 2015 - 2022
-                              </span>
-                            </div>
-                            <p className="mt-4 mb-2 pt-4 text-xl font-bold text-gray-600 dark:text-gray-300">
-                              Hannan University
-                            </p>
-                            <p className="text-gray-700 dark:text-gray-400">
-                              7+ years University Administrative Staff
-                            </p>
-                          </div>
-                        </div>
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="hidden absolute md:flex items-center justify-center w-8 h-8 transform -translate-x-1/2 -translate-y-4 bg-purple-100 border border-purple-600 rounded-full dark:border-gray-700 dark:bg-gray-700 left-1/2 sm:translate-y-0">
-                    <FaBagShopping className="text-purple-600 dark:text-gray-400 text-xl" />
-                  </div>
-                </div>
-
-                <div className="flex flex-col items-center">
                   <div className="flex items-center justify-end w-full mx-auto">
                     <div className="w-full lg:w-1/2 lg:pl-8">
                       <Link href="https://ciccc.ca/">
@@ -160,7 +70,7 @@ const Experience = ({ className = "" }) => {
                           <div className="relative z-20 p-6">
                             <div className="absolute -top-4 -right[-30px] lg:top-0 lg:right-0 inline-block px-2 py-2 dark:bg-purple-500 bg-purple-600 lg:rounded-bl-md lg:rounded-tr-md">
                               <span className="text-xs text-gray-100 ">
-                                July 2023 - December
+                                March 2023 - July
                               </span>
                             </div>
                             <p className="mt-4 mb-2 pt-4 text-xl font-bold text-gray-600 dark:text-gray-300">
